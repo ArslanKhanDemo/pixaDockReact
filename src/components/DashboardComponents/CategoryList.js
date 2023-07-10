5106dd05bf1a9906405836767192fa1707ea80c3
Arslan_Azhar <arslankhandemo@gmail.com>
2023-07-10 17:49:07 +0500

    In second_Branch_React

--- src/components/DashboardComponents/CategoryList.js
+++ src/components/DashboardComponents/CategoryList.js
@@ -1,88 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function CategoryList() {
+  const [categoryList, setCategoryList] = useState([]);
 
-    const [categoryList, setCategoryList] = useState([]);
-
-    const fetchData = async () => {
-        try {
-            const response = await fetch('http://localhost:5000/api/admin/catagories');
-            if (response.ok) {
-                const json = await response.json();
-                let Data = json.result.result;
-                setCategoryList(Data)
-                console.log("Data:fetch:", Data);
-
-            } else {
-                console.log("error");
-
-            }
-        } catch (error) {
-            console.log("error:", error);
-        }
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/admin/catagories');
+      if (response.ok) {
+        const json = await response.json();
+        let Data = json.result.result;
+        setCategoryList(Data);
+        console.log("Data:fetch:", Data);
+      } else {
+        throw new Error('Failed to fetch data');
+      }
+    } catch (error) {
+      console.log("error:", error);
     }
-    useEffect(() => {
-        fetchData();
-        console.log("useEffect: ", categoryList);
-    }, []);
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  let handleClick = (a) => {
+    console.log("Clicked", a);
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    console.log("categoryList:", categoryList);
-    return (
-
-        <>
-            <table className="table">
-                <tbody>
-                    {categoryList.map((value, index) => {
-                        return <tr>
-                            <td>
-                                <label htmlFor="firstName">Category Name</label>
-                                <input type="text" className="form-control" id="firstName" placeholder={`${value.categoryName}`} />
-                            </td>
-                            <td>
-                                <label htmlFor="lastName">Image</label>
-                                <input type="text" className="form-control" id="lastName" placeholder={`${value.image}`} />
-                            </td>
-                            <td>
-                                <label htmlFor="email">Parent</label>
-                                <input type="email" className="form-control" id="email" placeholder={`${value.parent}`} />
-                            </td>
-                            <td>
-                                <button type="submit" className="btn btn-primary mt-3">Submit</button>
-                            </td>
-
-                        </tr>
-
-                    })}
-
-                </tbody>
-            </table>
-
-
-        </>
-    )
+  return (
+    <>
+      <table className="table">
+        <tbody>
+          {categoryList.map((value, index) => (
+            <tr key={index}>
+              <td>
+                <label htmlFor="Category">Category Name</label>
+                <input type="text" className="form-control" id="Category" placeholder={`${value.categoryName}`} />
+              </td>
+              <td>
+                <label htmlFor="Image">Image</label>
+                <input type="text" className="form-control" id="Image" placeholder={`${value.image}`} />
+              </td>
+              <td>
+                <label htmlFor="Parent">Parent</label>
+                <input type="text" className="form-control" id="Parent" placeholder={`${value.parent}`} />
+              </td>
+              <td>
+                <button onClick={() => handleClick(value.parent)} className="btn btn-primary mt-3">Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default CategoryList
+export default CategoryList;
